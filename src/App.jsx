b777d3e270f0277f29a953acdd9d533c37a17bd0
phamyen1003 <nhovequehuong1003@gmail.com>
2024-06-23 09:28:35 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,38 @@ import {
   Routes,
   Route,
   Navigate,
+  useNavigate,
 } from "react-router-dom";
+import { useEffect } from "react";
 import ProtectedRoute from "./components/routers/ProtectedRoute";
-// import { useContext } from "react";
-// import { ProviderContext } from "./store/Provider";
 import Todo from "./components/Todo";
 import Login from "./components/Login";
 
 function App() {
-  // const { state, dispatch } = useContext(ProviderContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const apiKey = localStorage.getItem("apiKey");
+    if (apiKey) {
+      navigate("/todo");
+    }
+  }, [navigate]);
 
   return (
     <div className="w-full h-screen bg-gradient-to-r from-red-500 flex items-center justify-center">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/todo" element={<ProtectedRoute element={Todo} />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/todo" element={<ProtectedRoute element={Todo} />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
